47f29aa6f27ad2fb2912ce6daa499c2d577bdf37
biancaagan <biancaagan@gmail.com>
2023-02-10 15:58:58 -0500

    update

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-const express = require("express");
-const server = express();
-const bodyParser = require("body-parser");
-
-const portNum = 8111;
+var express = require('express');       // include express.js
+const res = require('express/lib/response');
+var server = express();         // a local instance of it
 
 
-// Serve alL the static files in the public folder:
-server.use('/gyro/', express.static("public"));
-// Use the body parser middleware:
-server.use(bodyParser.json());
-
-function serverStart(){
-    console.log("Server started on port " + portNum);
+function serverStart() {
+    console.log("Server started");
 }
 
-// Basic route
-server.get('/gyro/', (req, res) => {
-        res.send("Hello there.");
-    });
 
-// Listen for requests:
-server.listen(portNum, serverStart);
+// Start the server
+server.listen(8111, serverStart);
+
+
+// Routes
+// basic route
+server.get('/gyro', (req, res) => {
+    res.send("Hello there.\n");
+});
+
+
+
+
+
+
 
 
