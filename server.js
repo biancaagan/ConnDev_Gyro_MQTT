f4336f0a9b848181a3f21cc85070c1c009648f0e
biancaagan <biancaagan@gmail.com>
2023-02-10 16:16:13 -0500

    update

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
-var express = require('express');       // include express.js
-const res = require('express/lib/response');
-var server = express();         // a local instance of it
+const express = require("express");
+const server = express();
+
+const portNum = 8111;
 
 
-function serverStart() {
-    console.log("Server started");
+// Serve alL the static files in the public folder:
+//server.use('/gyro/', express.static("public"));
+
+
+function serverStart(){
+    console.log("Server started on port " + portNum);
 }
 
-
-// Start the server
-server.listen(8111, serverStart);
-
-
-// Routes
-// basic route
+// Basic route
 server.get('/gyro', (req, res) => {
-    res.send("Hello there.\n");
-});
+        res.send("Hello there.");
+    });
 
-
-
-
-
-
+// Listen for requests:
+server.listen(portNum, serverStart);
 
 
