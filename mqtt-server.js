aa5cf20d5ed751719b00e02df458e179c86e27e0
biancaagan <biancaagan@gmail.com>
2023-02-10 16:38:26 -0500

    update

--- mqtt-server.js
+++ mqtt-server.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const app = express();
 
-const portNum = 3000;
+const portNum = 8888;
 
 
 // Serve alL the static files in the public folder:
-//server.use('/gyro/', express.static("public"));
+app.use('/gyro/', express.static("public"));
 
 
 function serverStart(){
     console.log("Server started on port " + portNum);
 }
 
-// Basic route
-app.get('/gyro', (req, res) => {
-        res.send("Hello there.");
-    });
+// // Basic route
+// app.get('/gyro', (req, res) => {
+//         res.send("Hello there.");
+//     });
 
 // Listen for requests:
 app.listen(portNum, serverStart);
