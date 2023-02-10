f65eef3ac7aa94de20c8e9cb50f3226e5f6fef9d
biancaagan <biancaagan@gmail.com>
2023-02-10 15:30:22 -0500

    update

--- server.js
+++ server.js
@@ -4,21 +4,19 @@ const bodyParser = require("body-parser");
 
 const portNum = 8111;
 
-// JSON data to serve as a response, and to modify
-// when you get a POST request:
-var myData = {
-    sensor: 20
-};
 
 // Serve alL the static files in the public folder:
 server.use('/gyro/', express.static("public"));
-// Use the body parser middleware:
-server.use(bodyParser.json());
 
 
 function serverStart(){
     console.log("Server started on port " + portNum);
 }
+
+// Basic route
+server.get('/gyro', (req, res) => {
+        res.send("Hello there.");
+    });
 
 // Listen for requests:
 server.listen(portNum, serverStart);
